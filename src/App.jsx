087d036ff918a0cec0183491842ac06167fa1ac4
jayPreak <jayesh.preak2003@gmail.com>
2023-08-06 00:04:42 +0530

    Yea idk yet

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Environment, PointerLockControls, useFrame } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+
+export default function Game() {
+  return <div>Game</div>;
+}
